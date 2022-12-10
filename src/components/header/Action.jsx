aab8b0015a50c873779ebe8dd5175d68c82e512d
oliullah <razasm29@gmail.com>
2022-12-11 01:55:23 +0600

    added favicon and contact route

--- src/components/header/Action.jsx
+++ src/components/header/Action.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import resume from '../../assets/oliullah_resume.pdf';
 
 const Action = () => {
     return (
         <div className='actions'>
             <a href={resume} download className="btn">Download Resume</a>
-            <a href="#contact" className='btn-primary btn'>Talk With Me?</a>
+            <Link to="/contact" className='btn-primary btn'>Talk With Me?</Link>
         </div>
     );
 };
